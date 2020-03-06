113a7b4ec14e40ce9a9884d502f0478aaf74246b
innayugay <yuinnaa@gmail.com>
2020-03-06 19:24:18 +0000

    the game part is working, set interval is recording time every second

--- screens/participant/FlankerTask.js
+++ screens/participant/FlankerTask.js
@@ -9,49 +9,147 @@ export default function FlankerTask ({navigation}) {
 
     // const [currentTrial, setCurrentTrial] = useState()
     // var trialSequence = []
-    var currentTrial
-    var seconds = 0
+    // var currentTrial
+    // var seconds = 0
+    // var timer
+    var action
+    // const [seconds, setSeconds] = useState(0)
 
     const [isActive, setIsActive] = useState(false)
     const [trialSequence, setTrialSequence] = useState(navigation.getParam('arrs'))
-    // const [currentTrial, setCurrentTrial] = useState()
-    const [incongruentRT, setIncongruentRT] = useState()
+
     const [arrows, setArrows] = useState(' ')
-    // var trialSequence = navigation.getParam('arrs')
+    const [seconds, setSeconds] = useState(0)
+    const [isCorrect, setIsCorrect] = useState(true)
+    const [totalTime, setTotalTime] = useState([])
+
+    // const countSeconds = () =>{
+    //     seconds
+    // }
 
     useEffect(()=>{
         //initiate timer here?
         // setCurrentTrial(trialSequence[0])
         // currentTrial = trialSequence[0]
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=')
         renderArrows()
-        console.log(trialSequence, '=-=-=-=-=-=-=-=')
-        if (isActive){
-            console.log('timer gets called!')
-            setInterval( () => {
+        // startTimer()
+        // if (isActive){
+        //     console.log('timer gets called!')
+        //     setInterval( () => {
+        //         seconds = seconds + 1
+        //     },1000)
+        // }
+        var timer
+        var seconds = 0
+        console.log('in useeffect is active???? is', isActive)
+        if(isActive){
+            timer = setInterval(function(){
+                // setSeconds(seconds => seconds + 1)
                 seconds = seconds + 1
-            },1000)
+                console.log(seconds, 'seconds')
+            }, 1000)
         }
-    },[])
+        // else if (!isActive){
+        //     clearInterval(timer)
+        //     console.log(timer, 'timer')
+        // }
+        return ()=> {
+            clearInterval(timer)
+            console.log(seconds, 'is your seconds')
+            setTotalTime(totalTime=>[...totalTime, seconds])
+            console.log(totalTime, 'is your total time 0o0o0o0o0o0o0o0o0o0o0o')
+            // renderArrows()
+        }
+
+    },[isActive, isCorrect])
+
+    
+    // function controlTimer(){
+    //     var seconds = 0
+    //     if (timer){
+    //         console.log('stopped!')
+    //         clearInterval(timer)
+    //         timer = null
+    //         console.log('your reaction time is', seconds)
+    //         // seconds = 0
+    //     }
+    //     else{
+    //         timer = setInterval(function(){
+    //             console.log('time is running!!', seconds)
+    //             seconds = seconds + 1
+    //         }, 1000)
+    //     }
+    // }
 
 
-    function saveResults(){
-        //save the reaction times to the database
-    }
+
+    // function startTimer(){
+    //     // var timer = 0
+    //     console.log('timer is called', timer)
+    //     console.log(action, 'ACTION')
+    //     if (action == true){
+    //         var timer = setInterval(function(){
+    //             // setSeconds(seconds + 1)
+    //             console.log('starting the timer!!! ACTION IS', action)
+    //             seconds = seconds + 1
+    //             console.log(seconds, 'seconds!!')
+    //         },1000)
+    //         console.log(timer, 'this is what timer is')
+    //     }
+        
+    //     else{
+    //         console.log('stopping the timer!!!!', timer)
+    //         clearInterval(timer)
+    //         // timer = null
+    //         console.log('reaction time', seconds)
+    //     }
+    
+    // }
+
+    // function startTimer(){
+    //     console.log('timer starts!')
+    //     theTimer = setInterval(function(){
+    //         seconds = seconds + 1
+    //         console.log(seconds, 'seconds')
+    //     }, 1000)
+    // }
+
+    // function stopTimer(){
+    //     console.log('timer is stopped!', theTimer, 'this is timer')
+    //     clearInterval(theTimer)
+    // }
+
+
+    // function stopTimer(){
+    //     //save the reaction times to the database
+    //     console.log('stopping the timer!!!!')
+    //     clearInterval(timer)
+    //     alert('reaction time', seconds)     
+    // }
 
     function renderArrows (){
+        setIsActive(true)
+        console.log('in render arrows is active is', isActive)
 
         console.log(trialSequence.length, 'trials left')
         if (trialSequence.length > 0){
             // currentTrial = trialSequence[0]
 
             // setCurrentTrial(trialSequence[0])
-            console.log(currentTrial, 'heyheyhey')
+            console.log('heyheyhey')
+            // controlTimer()
+            // action = true
+            // startTimer()
+            // setIsActive(true)
             if (trialSequence[0].type === 'congruentRight'){
                 // setCurrentTrial('congruentRight')
                 // currentTrialType='congruentRight'
                 console.log('corrent answer is', trialSequence[0].correctAnswer)
                 setArrows('→   →   →   →   → ')
-                setIsActive(true)
+                // setIsActive(true)
+                
+                // startTimer()
                 // initiate timer here?
             }
             else if (trialSequence[0].type === 'congruentLeft'){
@@ -59,21 +157,30 @@ export default function FlankerTask ({navigation}) {
                 // currentTrialType='congruentRight'
                 console.log('corrent answer is', trialSequence[0].correctAnswer)
                 setArrows('←   ←    ←    ←    ←')
-                setIsActive(true)
+                // setIsActive(true)
+                // action = true
+                // startTimer()
+
                 // initiate timer here?
             }
             else if( trialSequence[0].type === 'incongruentRight'){
 
                 console.log('corrent answer is', trialSequence[0].correctAnswer)
                 setArrows(' ←   ←   →   ←   ←')
-                setIsActive(true)
+                // setIsActive(true)
+                // action = true
+                // startTimer()
+
                 // return render = <Text> ←    ←    →    ←    ← </Text>
             }
             else if( trialSequence[0].type === 'incongruentLeft'){
 
                 console.log('corrent answer is', trialSequence[0].correctAnswer)
                 setArrows('  →    →    ←    →   → ')
-                setIsActive(true)
+                // setIsActive(true)
+                // action = true
+                // startTimer()
+
                 // return render = <Text> ←    ←    →    ←    ← </Text>
             }
             
@@ -81,6 +188,9 @@ export default function FlankerTask ({navigation}) {
         else{
             setArrows('ok thats it')
             // call the saveResults
+            setIsActive(false)
+            console.log('your total time is', totalTime.reduce((a, b) => a + b, 0))
+            
         }
         
     }
@@ -105,38 +215,55 @@ export default function FlankerTask ({navigation}) {
 
     function checkResponseRight() {
         console.log('your answer is RIGHT')
+        // action = false
+        // startTimer()
+        // controlTimer()
         // currentTrial = trialSequence[0]
+        // setIsActive(false)
 
         if (trialSequence[0].correctAnswer === 'right'){
 
             // stop the timer, add it to the total time
             trialSequence.splice(0,1)
             console.log('you are correct and have' + trialSequence.length, 'trials left')
-            renderArrows()
+            // renderArrows()
+            setIsActive(false)
+
+
         }
         else if (trialSequence[0].correctAnswer === 'left') {
             //stop the timer, discard the value 
             // trialSequence.splice(0,1)
-            renderArrows()
+            // renderArrows()
             console.log('wrong it was left!!!!!')
+            setIsCorrect(false)
+
 
         }
     }
 
     function checkResponseLeft() {
         console.log('your answer is LEFT')
+        // action = false
+        // console.log('timer is', timer)
+        // startTimer()
+        // controlTimer()
+        // setIsActive(false)
         if (trialSequence[0].correctAnswer === 'left'){
 
             // stop the timer, add it to the total time
             trialSequence.splice(0,1)
+
             console.log('you are correct and have' + trialSequence.length, 'trials left')
-            renderArrows()
+            setIsActive(false)
+            // renderArrows()
         }
         else if (trialSequence[0].correctAnswer === 'right') {
             //stop the timer, discard the value 
             // trialSequence.splice(0,1)
-            renderArrows()
+            // renderArrows()
             console.log('wrong it was right!!!!!')
+            setIsCorrect(false)
 
         }
     }
